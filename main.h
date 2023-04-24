c13626a86191a388c42097536747c1c36c0e29e5
Fmukami <faithnjuguna43@gmail.com>
2023-04-24 11:10:58 -0700

    printf

--- main.h
+++ main.h
@@ -1,4 +1,19 @@
-#include main.h
+#include "main.h"
 
-*int _printf(const char *format, ...);
-*
+/**
+  * int _printf(const char *format, ...);
+  * @format: character string
+  * 
+  * Return: the number of characters printed (excluding the null byte used to end output to strings)
+  */
+int _printf(const char *format, ...);
+{	
+	
+         	fprintf( stdout, "number of characters printed (excluding the null byte used to end output to strings)" );
+		}
+               {
+		       if format : character string
+}
+
+	return (the number of characters printed (excluding the null byte used to end output to strings));
+}
